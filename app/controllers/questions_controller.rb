b9cfc2d7a5d16e98cdbe25902adc53389916a31e
Andres Ferrer <andferrer6a@gmail.com>
2022-11-14 12:42:03 +0100

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params['question']
+    if @question = 'I am going to work'
+      @answer = 'Great'
+    elsif @question.includes = '?'
+      @answer = 'Silly question, get to work'
+    else
+      @answer = 'I do not care, get dressed and go to work'
+    end
   end
 end
